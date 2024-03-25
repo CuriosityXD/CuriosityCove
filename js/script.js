01fe3cdecb6a16de5bc1f7308f398daf115c72b3
BuildTools <unconfigured@null.spigotmc.org>
2024-03-24 18:05:27 -0700

    commands

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 $(function() {
     $('body').terminal({
-        hello: function(what) {
-            this.echo('Hello [[gb;#fff;]person], ' + what +
-                '. Wellcome to this terminal.');
+        help: function() {
+            this.echo('\n"whois      who am I?')
+            this.echo('"help"      shows this menu')
+            this.echo('"banner"    shows start banner')
+            this.echo('"clear"     clears the terminal\n')
+        },
+        banner: function() {
+            this.echo(intro.innerHTML)
+        },
+        whois: function() {
+            this.echo('\n' + whoisTxt.innerHTML + '\n')
         }
     }, {
         greetings: intro.innerHTML
