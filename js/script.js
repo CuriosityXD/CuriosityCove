82e8da2149fe8d4dd673b56a583b3b86680c0a59
BuildTools <unconfigured@null.spigotmc.org>
2024-03-24 21:30:48 -0700

    animation

--- js/script.js
+++ js/script.js
@@ -1,20 +1,62 @@
 $(function() {
     var term = $('body').terminal({
         help: function() {
-            this.echo('\n"whois      who am I?')
-            this.echo('"help"      shows this menu')
-            this.echo('"banner"    shows start banner')
-            this.echo('"clear"     clears the terminal\n')
+            this.echo('\n"whois"     who am I?');
+            this.echo('"help"      shows this menu');
+            this.echo('"banner"    shows start banner');
+            this.echo('"clear"     clears the terminal\n');
         },
         banner: function() {
-            this.echo(intro.innerHTML)
+            var self = this;
+            this.pause();
+            var content = intro.innerHTML;
+            typed_message(self, content, 0.1, function() {
+                self.echo(''); 
+                self.resume();
+            });
         },
         whois: function() {
-            this.echo(whoisTxt.innerHTML)
+            var self = this;
+            this.pause();
+            var content = whoisTxt.innerHTML;
+            typed_message(self, content, 0.1, function() {
+                self.echo(''); 
+                self.resume();
+            });
         }
     }, {
         greetings: intro.innerHTML
     });
-});
 
-$("body").css("background-color", "white");
+    var anim = false;
+
+    function typed(finish_typing) {
+        return function(term, message, delay, finished, classname) {
+            anim = true;
+            var prompt = term.get_prompt();
+            var c = 0;
+            if (message && message.length > 0) {
+                term.set_prompt('');
+                var interval = setInterval(function() {
+                    if (c < message.length) {
+                        term.insert(message[c++]);
+                    } else {
+                        clearInterval(interval);
+                        setTimeout(function() {
+                            finish_typing(term, message, prompt, classname);
+                            anim = false;
+                            finished && finished();
+                        }, delay);
+                    }
+                }, delay);
+            }
+        };
+    }
+
+    var typed_message = typed(function(term, message, prompt, classname) {
+        if (typeof classname === "undefined") { classname = "default"; }
+        term.set_command('');
+        term.echo(message, { finalize: function(div) { div.addClass(classname); } });
+        term.set_prompt(prompt);
+    });
+});
